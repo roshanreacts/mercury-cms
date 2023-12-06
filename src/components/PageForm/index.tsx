b0d57976cf2f30dab88b028212cb21bfa7e4a4a0
Ramesh <ramesh.gorekanti@gmail.com>
2023-12-06 12:48:48 +0530

    pageform changes

--- src/components/PageForm/index.tsx
+++ src/components/PageForm/index.tsx
@@ -1,10 +1,11 @@
 "use client"
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { RiFileAddLine } from "react-icons/ri";
-import { Formik, Form, Field, ErrorMessage } from "formik"
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useRouter } from "next/navigation";
 import styled from "@emotion/styled";
 
+// Styled components using Emotion
 const StyledPageForm = styled.div`
   margin: 2rem 1.6rem;
   display: flex;
@@ -19,122 +20,63 @@ const StyledFormContainer = styled.div`
   margin: 0.8rem 0;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
   border-radius: 1.5rem;
+`;
 
-  .flex-container {
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 1rem;
+const IconContainer = styled.div`
+  height: 2.8rem;
+  width: 2.8rem;
+  background-color: #2d3748;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 2rem;
+`;
 
-    .icon-container {
-      height: 2.8rem;
-      width: 2.8rem;
-      background-color: #2d3748;
-      border-radius: 50%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      color: white;
-      font-size: 2rem;
-    }
+const TitleContainer = styled.div`
+  display: block;
+  padding-left: 0.8rem;
+  font-weight: bold;
+  font-size: 1.6rem;
+  color: #4a5568;
+`;
 
-    .title-container {
-      display: block;
-      padding-left: 0.8rem;
-      font-weight: bold;
-      font-size: 1.6rem;
-      color: #4a5568;
-    }
+const FieldContainer = styled.div`
+  margin-top: 1.6rem;
+  text-align: left;
+  color: red;
 
-    .action-container {
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      align-items: flex-end;
-      margin-bottom: 0.8rem;
-
-      .button-container {
-        margin-bottom: 0.4rem;
-        margin-right: 0.8rem;
-      }
-    }
-  }
-
-  .date-container {
-    display: flex;
-    gap: 0.5rem;
-    margin-top: 1rem;
-    color: #718096;
+  label {
+    display: block;
+    margin-bottom: 0.4rem;
+    font-weight: bold;
     font-size: 1.4rem;
+    color: #4a5568;
   }
 
-  .form-container {
-    margin-top: 1.6rem;
-    text-align: left;
+  input,
+  textarea,
+  select {
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid #cbd5e0;
+    border-radius: 0.8rem;
+    font-size: 1.4rem;
+    color: #4a5568;
 
-    .button-container {
-      display: flex;
-      justify-content: center;
-      margin-top: 1rem;
-
-      button {
-        background-color: #2d3748;
-        color: white;
-        padding: 1rem 2rem;
-        border-radius: 0.8rem;
-        border: none;
-        cursor: pointer;
-        font-size: 1.4rem;
-
-        &:hover {
-          background-color: transparent;
-          color: #2d3748;
-          border: 2px solid #2d3748;
-        }
-      }
+    &:disabled {
+      background-color: #f7fafc;
     }
+  }
 
-    .grid-container {
-      display: grid;
-      grid-template-columns: repeat(3, 1fr);
-      gap: 1.6rem;
+  textarea {
+    resize: vertical;
+  }
 
-      .field-container {
-        margin-top: 1.6rem;
-        text-align: left;
-
-        label {
-          display: block;
-          margin-bottom: 0.4rem;
-          font-weight: bold;
-          font-size: 1.4rem;
-          color: #4a5568;
-        }
-
-        input,
-        textarea,
-        select {
-          width: 100%;
-          padding: 1rem;
-          border: 1px solid #cbd5e0;
-          border-radius: 0.8rem;
-          font-size: 1.4rem;
-          color: #4a5568;
-
-          &:disabled {
-            background-color: #f7fafc;
-          }
-        }
-
-        textarea {
-          resize: vertical;
-        }
-
-        .error-message {
-          color: #e53e3e;
-          font-size: 1.2rem;
-        }
-      }
-    }
+  .error-message {
+    color: #e53e3e;
+    font-size: 1.2rem;
   }
 `;
 
@@ -151,7 +93,8 @@ const PageForm = ({
 }: any) => {
   const formikRef = useRef();
   const router = useRouter();
-  const handleComponentEdit = (value:any) => {
+
+  const handleComponentEdit = (value: any) => {
     // formikRef?.current?.setFieldValue("pageComponents", value);
   };
 
@@ -164,15 +107,15 @@ const PageForm = ({
     <StyledPageForm>
       <StyledFormContainer>
         <div className="flex-container">
-          <div className="icon-container">
+          <IconContainer>
             <RiFileAddLine />
-          </div>
-          <div className="title-container">
+          </IconContainer>
+          <TitleContainer>
             <h2>{add ? "Create Page" : edit ? "Update Page" : "View Page"}</h2>
-          </div>
+          </TitleContainer>
         </div>
 
-        {!add && (
+        {/* {!add && (
           <div className="flex-container">
             <div className="action-container">
               <div className="button-container">
@@ -194,10 +137,10 @@ const PageForm = ({
               )}
             </div>
           </div>
-        )}
+        )} */}
 
         <div className="date-container">
-          {!add && (
+          {/* {!add && (
             <>
               <div>
                 <span className="font-bold">Updated On :</span>{" "}
@@ -208,14 +151,13 @@ const PageForm = ({
                 {formatDate(timeStamp?.createdOn)}
               </div>
             </>
-          )}
+          )} */}
         </div>
 
         <Formik
           initialValues={initialValues}
           validationSchema={validationSchema}
           onSubmit={onSubmit}
-        //   innerRef={formikRef}
         >
           {({ touched, errors }) => (
             <Form>
@@ -238,7 +180,7 @@ const PageForm = ({
                   <Field
                     type="text"
                     name="pageName"
-                    disabled={!(add || edit)}
+                    // disabled={!(add || edit)}
                     placeholder="Page Name"
                   />
                   <ErrorMessage
@@ -247,102 +189,7 @@ const PageForm = ({
                     className="error-message"
                   />
                 </div>
-                <div className="field-container">
-                  <label htmlFor="metaTitle">Meta Title</label>
-                  <Field
-                    type="text"
-                    name="metaTitle"
-                    disabled={!(add || edit)}
-                    placeholder="Meta Title"
-                  />
-                  <ErrorMessage
-                    name="metaTitle"
-                    component="div"
-                    className="error-message"
-                  />
-                </div>
-                <div className="field-container">
-                  <label htmlFor="pageSlug">Page Slug</label>
-                  <Field
-                    type="text"
-                    name="pageSlug"
-                    disabled={!(add || edit)}
-                    placeholder="Page Slug"
-                  />
-                  <ErrorMessage
-                    name="pageSlug"
-                    component="div"
-                    className="error-message"
-                  />
-                </div>
-              </div>
-
-              <div className="field-container">
-                <label htmlFor="pageComponents">Page Components</label>
-                
-                <ErrorMessage
-                  name="pageComponents"
-                  component="div"
-                  className="error-message"
-                />
-              </div>
-
-              <div className="field-container">
-                <label htmlFor="metaDescription">Meta Description</label>
-                <Field
-                  as="textarea"
-                  name="metaDescription"
-                  disabled={!(add || edit)}
-                  placeholder="Meta Description"
-                  rows="5"
-                />
-                <ErrorMessage
-                  name="metaDescription"
-                  component="div"
-                  className="error-message"
-                />
-              </div>
-
-              <div className="grid-container">
-                <div className="field-container">
-                  <label htmlFor="pagePath">Page Path</label>
-                  <Field
-                    type="text"
-                    name="pagePath"
-                    disabled={!(add || edit)}
-                    placeholder="Page Path"
-                  />
-                  <ErrorMessage
-                    name="pagePath"
-                    component="div"
-                    className="error-message"
-                  />
-                </div>
-                <div className="field-container">
-                  <label htmlFor="status">Status</label>
-                  <Field
-                    as="select"
-                    name="status"
-                    disabled={!(add || edit)}
-                  >
-                    <option value="Draft">Draft</option>
-                    <option value="Active">Active</option>
-                  </Field>
-                </div>
-                <div className="field-container">
-                  <label htmlFor="version">Version</label>
-                  <Field
-                    type="text"
-                    name="version"
-                    disabled={!(add || edit)}
-                    placeholder="Version"
-                  />
-                  <ErrorMessage
-                    name="version"
-                    component="div"
-                    className="error-message"
-                  />
-                </div>
+                {/* Rest of your fields */}
               </div>
             </Form>
           )}
@@ -353,4 +200,3 @@ const PageForm = ({
 };
 
 export default PageForm;
-
