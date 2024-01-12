e3afeec43d770c487dd27a975af83c59ab0278c4
Praveen Vuddagiri <vuddagiripraveen@gmail.com>
2024-01-12 15:16:03 +0530

    added read time for blogs

--- src/components/BlogListCard.tsx
+++ src/components/BlogListCard.tsx
@@ -1,10 +1,8 @@
+import readTime from "@/utils/methods";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import React from "react";
-import { FiEdit } from "react-icons/fi";
 import { LiaReadme } from "react-icons/lia";
-import { MdOutlineDeleteOutline, MdOutlineEditCalendar } from "react-icons/md";
 import { RiTimer2Line } from "react-icons/ri";
 
 const BlogListCard = ({
@@ -15,49 +13,33 @@ const BlogListCard = ({
   id,
   content,
 }: any) => {
-  const router = useRouter();
   return (
-    <div className="relative group hover:scale-105 ease-in duration-300">
+    <div className="relative">
       <div className="mx-auto h-[480px] w-80 bg-white shadow-md border border-gray-200 rounded-lg mb-5">
         <div className="">
-          <div className="relative">
-            <Link href="#" className="">
-              <Image
-                className="rounded-t-lg w-80 h-60 object-cover"
-                src={imgSrc}
-                alt="image"
-                height={1000}
-                width={1000}
-              />
-            </Link>
-            <div className="absolute top-2 right-2 flex gap-1 opacity-0 group-hover:opacity-100 transition-opacity">
-              <button
-                className="px-2 py-1 bg-blue-400 hover:bg-blue-900 rounded hover:text-white text-[10px]"
-                onClick={() => {
-                  router.push(`blog/${id}?edit=true`);
-                }}
-              >
-                <span>  <FiEdit className="w-4 h-4" /></span>
-              
-              </button>
-              <button className="px-2 py-1 bg-red-500 hover:bg-red-900 rounded hover:text-white text-[10px]">
-              <span>  <MdOutlineDeleteOutline className="w-4 h-4" /></span>
-              </button>
-            </div>
-          </div>
-
+          <Link href="#" className="">
+            <Image
+              className="rounded-t-lg w-80 h-60 object-cover"
+              src={imgSrc}
+              alt="image"
+              height={1000}
+              width={1000}
+            />
+          </Link>
           <div className="p-5 pb-2 flex justify-start items-start flex-col">
             <div className="flex justify-between text-[12px] mb-2 w-full">
               <h6 className="flex justify-center items-center gap-1">
                 <RiTimer2Line />
                 {new Date(created).toDateString().split(" ").slice(1).join(" ")}
               </h6>
-              <p>
+              <p className="flex justify-center items-center gap-1">
                 <LiaReadme className="w-5 h-5" />
+                {readTime(content)}
               </p>
             </div>
             <div className="overflow-hidden w-full h-full">
-              <Link href={`blog/${id}?view=true`}>
+              <Link
+                href={`blog/${id}`}>
                 <h5 className="text-gray-900 font-bold text-xl tracking-tight mb-2 hover:underline  overflow-hidden line-clamp-2">
                   {heading}
                 </h5>
