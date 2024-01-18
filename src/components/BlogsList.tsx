28295a8317d1d0fa2afe1b51fa0e7b4cd093a5b0
Ramesh <ramesh.gorekanti@gmail.com>
2024-01-18 17:07:03 +0530

    blog card changes

--- src/components/BlogsList.tsx
+++ src/components/BlogsList.tsx
@@ -59,7 +59,7 @@ const BlogsList = ({
               >
                 <button className="bg-white text-blue-500 hover:text-blue-400 hover:underline">Read more</button>
               </Link>
-              <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+              {/* <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
                 <svg
                   className="w-4 h-4 mr-1"
                   viewBox="0 0 24 24"
@@ -86,7 +86,7 @@ const BlogsList = ({
                   <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
                 </svg>
                 89
-              </span>
+              </span> */}
             </div>
           </div>
         </div>
